b34bca426906cde4b460a98f992efd359d7779db
duanjincheng <1421759951@qq.com>
2026-02-27 17:28:27 +0800

    棋子生成

--- assets/script/PieceController.ts
+++ assets/script/PieceController.ts
@@ -1,78 +1,102 @@
-import { _decorator, Component, Node, Sprite, Color, Label } from 'cc';
-const { ccclass, property } = _decorator;
+import { _decorator, Component, Node, Sprite, Color, Label } from 'cc'
+const { ccclass, property } = _decorator
 
 type PieceInfo = {
-    value: number
-    bColor: string
+  value: number
+  bColor: string
 }
 
 @ccclass('PieceController')
 export class PieceController extends Component {
-
-    pieceInfoList: PieceInfo[] = [
-        {
-            value: 2,
-            bColor: '#ff89d1'
-        },
-        {
-            value: 4,
-            bColor: '#7c55fa'
-        },
-        {
-            value: 8,
-            bColor: '#a55fd4'
-        },
-        {
-            value: 16,
-            bColor: '#14e2de'
-        },
-        {
-            value: 32,
-            bColor: '#62ccfe'
-        },
-        {
-            value: 64,
-            bColor: '#89e66c'
-        },
-        {
-            value: 128,
-            bColor: '#f87275'
-        },
-        {
-            value: 256,
-            bColor: '#a46f62'
-        },
-        {
-            value: 512,
-            bColor: '#8d949c'
-        },
-        {
-            value: 1024,
-            bColor: '#ffb659'
-        },
-    ]
-    start() {
-
+  pieceInfoList: PieceInfo[] = [
+    {
+      value: 2,
+      bColor: '#ff89d1'
+    },
+    {
+      value: 4,
+      bColor: '#7c55fa'
+    },
+    {
+      value: 8,
+      bColor: '#a55fd4'
+    },
+    {
+      value: 16,
+      bColor: '#14e2de'
+    },
+    {
+      value: 32,
+      bColor: '#62ccfe'
+    },
+    {
+      value: 64,
+      bColor: '#89e66c'
+    },
+    {
+      value: 128,
+      bColor: '#f87275'
+    },
+    {
+      value: 256,
+      bColor: '#a46f62'
+    },
+    {
+      value: 512,
+      bColor: '#8d949c'
+    },
+    {
+      value: 1024,
+      bColor: '#ffb659'
     }
+  ]
 
-    update(deltaTime: number) {
-        
+  @property
+  fallSpeed: number = 200
+
+  @property
+  fastSpeed: number = 500
+
+  private currentSpeed: number = 200
+  private isFalling: boolean = true
+
+  start() {}
+
+  update(dt: number) {
+    if (!this.isFalling) return
+
+    const pos = this.node.position
+
+    const newY = pos.y - this.currentSpeed * dt
+
+    this.node.setPosition(pos.x, newY)
+  }
+
+  startFastDrop() {
+    this.currentSpeed = this.fastSpeed
+  }
+
+  stopFastDrop() {
+    this.currentSpeed = this.fallSpeed
+  }
+
+  stopFalling() {
+    this.isFalling = false
+  }
+
+  setValue(value: number) {
+    const pieceInfo = this.pieceInfoList.find(item => item.value === value)
+    if (!pieceInfo) {
+      return
     }
+    const _sprite = this.node.getComponent(Sprite)
+    const _value = this.node.getChildByName('Value')
+    const color = new Color()
+    Color.fromHEX(color, pieceInfo.bColor)
+    _sprite.color = color
 
-    setValue(value: number) {
-        const pieceInfo = this.pieceInfoList.find(item => item.value === value)
-        if (!pieceInfo) {
-            return
-        }
-        const _sprite = this.node.getComponent(Sprite)
-        const _value = this.node.getChildByName('Value')
-        const color = new Color()
-        Color.fromHEX(color, pieceInfo.bColor); 
-        _sprite.color = color
-
-        const _valueLabel = _value.getComponent(Label)
-        _valueLabel.string = value.toString()
-        console.log('__', _valueLabel)
-    }
+    const _valueLabel = _value.getComponent(Label)
+    _valueLabel.string = value.toString()
+    console.log('__', _valueLabel)
+  }
 }
-
--- assets/script/PlayController.ts
+++ assets/script/PlayController.ts
@@ -1,33 +1,69 @@
-import { _decorator, Component, Node, Prefab, instantiate } from 'cc';
-import { PieceController } from './PieceController';
-const { ccclass, property } = _decorator;
+import { _decorator, Component, Node, Prefab, instantiate } from 'cc'
+import { PieceController } from './PieceController'
+const { ccclass, property } = _decorator
 
 @ccclass('PlayController')
 export class PlayController extends Component {
-    
-    @property({
-        type: Prefab,
-        tooltip: '基础棋子控制器',
-    })
-    basePieceController: Prefab = null
+  @property({
+    tooltip: '棋盘宽度，单位格子数'
+  })
+  boardwidth: number = 5
+  @property({
+    tooltip: '棋盘高度，单位格子数'
+  })
+  boardheight: number = 7
 
-    start() {
-        for (let i = 0; i < 10; i++) {
-            const pieceNode = this.createPiece(Math.pow(2, i + 1))
-            pieceNode.setPosition(i * 100, i * 100)
-            this.node.addChild(pieceNode)
-        }
-    }
+  @property({
+    type: Prefab,
+    tooltip: '基础棋子控制器'
+  })
+  basePieceController: Prefab = null
 
-    update(deltaTime: number) {
-        
-    }
+  basePieceList: number[] = [2, 4, 8, 16, 32, 64, 128]
+  startPosition: { x: number; y: number } = { x: 0, y: 0 }
 
-    createPiece(val: number) {
-        const pieceNode = instantiate(this.basePieceController)
-        const pieceController = pieceNode.getComponent(PieceController)
-        pieceController.setValue(val)
-        return pieceNode
+  currentPiece: PieceController = null
+
+  start() {
+    this.generatePiece()
+  }
+
+  update(deltaTime: number) {
+    // 判断当前棋子是否可以继续下落
+    if (this.currentPiece) {
+      if (this.checkCurrentFall()) {
+        this.currentPiece.stopFalling()
+        this.currentPiece = null
+        // 生成新的棋子
+        this.generatePiece()
+      }
     }
+  }
+
+  /**
+   * 判断当前棋子是否可以继续下落
+   * 根据棋盘格子数量进行判断，如果下方的格子为1时，说明已经有棋子了，不能继续下落了
+   * 如果下方的格子为0时，说明没有棋子了，可以继续下落了
+   * 
+   * 如果是最后一行，说明已经到了底部，不能继续下落了
+   * @returns 
+   */
+  checkCurrentFall(): boolean {
+    return this.currentPiece.node.position.y <= -300
+  }
+
+  generatePiece() {
+    const index = Math.floor(Math.random() * this.basePieceList.length)
+    const pieceNode = this.createPiece(this.basePieceList[index])
+    pieceNode.setPosition(this.startPosition.x, this.startPosition.y)
+    this.node.addChild(pieceNode)
+    this.currentPiece = pieceNode.getComponent(PieceController)
+  }
+
+  createPiece(val: number) {
+    const pieceNode = instantiate(this.basePieceController)
+    const pieceController = pieceNode.getComponent(PieceController)
+    pieceController.setValue(val)
+    return pieceNode
+  }
 }
-
